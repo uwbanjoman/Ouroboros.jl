67e72d251a8007c129b638571e211dd41eb1be77
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2025-10-07 21:00:49 +0200

    Update fields.jl

--- src/fields.jl
+++ src/fields.jl
@@ -4,7 +4,7 @@
 struct Field3D
     C::CuArray{Float32,3}      # Consciousness
     Q::CuArray{Float32,3}      # Dual field / matter
-    I::CuArray{SVector{3,Float32},3}  # Interaction / energy flow (vector field)
+    I::CuArray{SVector{3,Float32},4}  # Interaction / energy flow (vector field)
     ρ::CuArray{Float32,3}      # Charge / density
     τ::CuArray{Float32,3}      # Experienced time
     extras::Dict{Symbol,CuArray{Float32,3}} # domain specific extra fields
