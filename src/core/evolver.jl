278464f9ff19747294ccfdce6dbb979f6538d423
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2025-10-09 20:02:33 +0200

    Update evolver.jl

--- src/core/evolver.jl
+++ src/core/evolver.jl
@@ -1,1 +1,16 @@
 
+using ..Registry
+
+export evolve_step!
+
+"""
+    evolve_step!(F::Field3D, Δt::Float32)
+
+Calls all registered physics modules on the field F for a single timestep.
+"""
+function evolve_step!(F, Δt::Float32)
+    for (_, update_fn) in Registry.get_modules()
+        update_fn(F, Δt)   # each module updates the field
+    end
+end
+
