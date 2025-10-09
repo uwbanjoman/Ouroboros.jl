0af551eba1d4e43f7be71bbf14e383cc26b3ad45
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2025-10-09 20:01:28 +0200

    Update registry.jl

--- src/core/registry.jl
+++ src/core/registry.jl
@@ -1,1 +1,21 @@
 
+export register_module!, get_modules
+
+const MODULES = Dict{Symbol, Function}()
+
+"""
+    register_module!(name::Symbol, update_fn::Function)
+
+Registers a physics module by name.
+`update_fn(F::Field3D, Δt)` will be called each evolution step.
+"""
+function register_module!(name::Symbol, update_fn::Function)
+    MODULES[name] = update_fn
+end
+
+"""
+    get_modules() -> Dict{Symbol, Function}
+
+Returns all registered modules.
+"""
+get_modules() = MODULES
